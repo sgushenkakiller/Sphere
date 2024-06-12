05498837d25641f8d5175ed0063c05b2760b79ff
sgushenkakiller <144253878+sgushenkakiller@users.noreply.github.com>
2024-06-12 21:49:19 +0300

    Update Point.h

--- Library/Point.h
+++ Library/Point.h
@@ -15,11 +15,11 @@ struct Point {
         is >> point.x >> point.y >> point.z;
         return is;
     }
-    int operator==(const Point &other) {
+    bool operator==(const Point &other) {
         if (this->x == other.x && this->y == other.y && this->z == other.z) return 1;
         return 0;
     }
-    int operator!=(const Point &other) {
+    bool operator!=(const Point &other) {
         if (this->x != other.x || this->y != other.y || this->z != other.z) return 1;
         return 0;
     }
